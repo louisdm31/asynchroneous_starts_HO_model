a713012067e2720a8d25117e5144fa0663170427
Louis de Monterno <louis.penetdemonterno@gmail.com>
2020-04-20 16:32:14 +0200

    lemme des tiroirs

--- modélisation Isabelle/otr/OneThirdRuleProof.thy
+++ modélisation Isabelle/otr/OneThirdRuleProof.thy
@@ -26,6 +26,88 @@ definition VInv :: "(nat \<Rightarrow> Proc \<Rightarrow> ('val::linorder) pstat
    in \<forall>p. rho n p \<noteq> Aslept \<longrightarrow> (\<exists>s. rho n p = Active s \<and> x      s \<in> xinit)
                            \<longrightarrow> (\<exists>s. rho n p = Active s \<and> decide s \<in> {None} \<union> (Some ` xinit))"
 
+lemma tiroir: assumes fini:"finite {v. E v}" shows "finite {f v | v. E v}"
+proof -
+  have "card {v. E v} \<ge> 0" (is "?cardens \<ge> 0") by auto
+  thus "finite {f v | v. E v}"
+  proof (induction ?cardens)
+    case 0
+    hence "{v. E v} = {}" using fini by auto
+    hence "{f v | v. E v} = {}" by auto
+    thus "finite {f v | v. E v}"  by auto
+  next
+    case (Suc crd)
+    hence "card {v. E v} > 0" by auto
+    hence "\<exists>vv. vv \<in> {v. E v}"
+      by (metis less_irrefl sum_bounded_above_strict sum_constant)
+    then obtain vv where vvin:"vv \<in> {v. E v}" by auto
+    have "\<forall>v. E v = (\<lambda>t. if t = vv then True else E t) v" using vvin by simp
+    hence "{v. E v} = {v. (\<lambda>t. if t = vv then True else E t) v}" by blast
+    hence dehorsvv:"{v. E v} = {v. (\<lambda>t. if t = vv then False else E t) v} \<union> {vv}" by auto
+    hence "finite {v. (\<lambda>t. if t = vv then False else E t) v}" using fini by auto
+    hence "finite {f v | v. (\<lambda>t. if t = vv then False else E t) v}" by auto
+    hence "finite ({f v | v. (\<lambda>t. if t = vv then False else E t) v} \<union> {f vv})" by auto
+    moreover have "{f v | v. E v} = {f v | v. (\<lambda>t. if t = vv then False else E t) v} \<union> {f vv}"
+      using vvin by auto
+    ultimately show "finite {f v | v. E v}" by simp
+  qed
+qed
+
+lemma tiroir2: assumes inject:"\<forall>v1 v2. f v1 = f v2 \<longrightarrow> v1 = v2"
+  shows "finite {f v | v. E v} \<longrightarrow> finite {v. E v}"
+proof -
+  have "card {f v | v. E v} \<ge> 0" (is "?cardens \<ge> 0") by auto
+  show ?thesis
+  proof (induction ?cardens arbitrary:E)
+    case 0
+    hence zercrd:"card {f v | v. E v} = 0" by auto
+    show ?case 
+    proof
+      assume fini:"finite {f v | v. E v}"
+      hence "{f v | v. E v} = {}"  using zercrd by auto
+      thus "finite {v. E v}" by auto
+    qed
+  next
+    case (Suc crd)
+    show ?case proof assume "finite {f v | v. E v}"
+    hence succrd:"card {f v | v. E v} = Suc crd" using Suc.hyps by auto
+    hence nonvoid:"card {f v | v. E v} > 0" by auto
+    hence finii:"finite {f v | v. E v}" using card_ge_0_finite by blast
+    hence "\<exists>vv. vv \<in> {f v | v. E v}" using nonvoid by (simp add: card_gt_0_iff)
+    then obtain vv where vvin:"vv \<in> {v. E v}" by auto
+    have "\<forall>v. E v = (\<lambda>t. if t = vv then True else E t) v" using vvin by simp
+    hence "{f v | v. E v} = {f v | v. (\<lambda>t. if t = vv then True else E t) v}" by blast
+    hence dehorsvv:"{f v | v. E v} = {f  v | v. (\<lambda>t. if t = vv then False else E t) v} \<union> {f vv}" by auto
+    hence finiii:"finite {f  v | v. (\<lambda>t. if t = vv then False else E t) v}" using finii by auto
+
+    have "f vv \<notin> {f  v | v. (\<lambda>t. if t = vv then False else E t) v}"
+    proof
+      assume "f vv \<in> {f v | v. (\<lambda>t. if t = vv then False else E t) v}"
+      hence "\<exists>vv2. f vv2 = f vv \<and>  (\<lambda>t. if t = vv then False else E t) vv2" by (smt mem_Collect_eq)
+      then obtain vv2 where exvv2:"f vv2 = f vv \<and>  (\<lambda>t. if t = vv then False else E t) vv2" by blast
+      hence "vv2 = vv" using inject by auto
+      hence " (\<lambda>t. if t = vv then False else E t) vv" using exvv2 by auto
+      thus "False" by simp
+    qed
+    hence "{f v | v. (\<lambda>t. if t = vv then False else E t) v} \<inter> {f vv} = {}" by auto
+    hence "card {f v | v. E v} = card {f  v | v. (\<lambda>t. if t = vv then False else E t) v} + 1"
+      using dehorsvv
+      by (metis (no_types, lifting) One_nat_def Un_insert_right
+          \<open>0 < card {f v |v. E v}\<close> \<open>f vv \<notin> {f v |v. if v = vv then False else E v}\<close>
+          add.right_neutral add_Suc_right card_infinite card_insert_disjoint finite_Un
+          less_le sup_bot.right_neutral)
+    have  "{f  v | v. (\<lambda>t. if t = vv then False else E t) v} \<subseteq> {f v | v. E v}" using dehorsvv by auto
+    hence "finite {f  v | v. (\<lambda>t. if t = vv then False else E t) v}" using finiii by blast
+    moreover have "card {f  v | v. (\<lambda>t. if t = vv then False else E t) v} = crd" using succrd
+      by (simp add: \<open>card {f v |v. E v} = card {f v |v. if v = vv then False else E v} + 1\<close>)
+    ultimately have "finite {v. (\<lambda>t. if t = vv then False else E t) v}" using Suc.hyps by auto
+    hence "finite ({v. (\<lambda>t. if t = vv then False else E t) v} \<union> {vv})" by auto
+    moreover have "{v. E v} = {v. (\<lambda>t. if t = vv then False else E t) v} \<union> {vv}"
+      using vvin by auto
+    ultimately show "finite {v. E v}" by simp
+  qed
+qed qed
+
 lemma vinv_invariant:
   assumes not_inf:"\<forall>p. \<exists>n. rho n p \<noteq> Aslept"
   and run:"HORun (\<lparr> CinitState = CinitState OTR_M, sendMsg = sendMsg OTR_M, CnextState = CnextState OTR_M \<rparr>
@@ -137,22 +219,21 @@ proof -
                     hence "?ens \<subseteq> {qq. \<exists>pp. ?rcvd pp = Content qq}" by auto
                     hence "infinite {qq. \<exists>pp. ?rcvd pp = Content qq}"
                       using infens rev_finite_subset by blast
-                    hence "False"
-                    proof
-                      have "finite {pp :: Proc. True}" by auto
-                      hence "finite {v. \<exists>p. ?rcvd p = v}" by (smt Collect_cong finite_image_set)
-                      hence "\<not> infinite {v. \<exists>p. ?rcvd p = Content v}"
-                      proof
-                        assume "infinite {v. \<exists>p. ?rcvd p = Content v}"
-                        hence  "infinite (Content` {v. \<exists>p. ?rcvd p = Content v})"
-                          by sledgehamme
-                        moreover have "Content` {v. \<exists>p. ?rcvd p = Content v} \<subseteq> {v. \<exists>p. ?rcvd p = v}"
-                          by blast
-                        ultimately have "infinite {v. \<exists>p. ?rcvd p = v}"
+
+                    have "finite {pp :: Proc. True}" by auto
+                    hence "finite {v. \<exists>p. ?rcvd p = v}" by (smt Collect_cong finite_image_set)
+                    moreover have "{Content v | v.  \<exists>p. ?rcvd p = Content v} \<subseteq> {v. \<exists>p. ?rcvd p = v}"
+                      by blast
+                    ultimately have fincontens:"finite {Content v | v.  \<exists>p. ?rcvd p = Content v}"
+                      by (meson rev_finite_subset)
+                    moreover have "\<forall>vv1 vv2. Content vv1 = Content vv2 \<longrightarrow> vv1 = vv2" by auto
+                    ultimately have "finite {v. \<exists>p. ?rcvd p = Content v}" using tiroir2  by auto
+
+                   
                     hence "infinite {pp. \<exists>qq. ?rcvd pp = Content qq}"
                   qed
 
-                  hence "\<forall>v. \<not> (MFR ?rcvd v)" by blast
+                  hence "\<forall>v. \<not> (MFR ?rcvd v)" by blas
                   hence "\<forall>vv vvv. card (HOV ?rcvd vv) \<le> card (HOV ?rcvd vvv)" unfolding MFR_def by auto
                   have "s \<noteq> ss" using dans by auto
                   hence "False" unfolding Min_def using dans by force
