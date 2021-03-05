bf58a69c0d96f833c821331f5bd1b27f1ae9392c
Louis de Monterno <louis.penetdemonterno@gmail.com>
2021-03-05 10:10:58 +0100

    avancement Views.thy

--- proof_Isabelle/sync-mod/Views.thy
+++ proof_Isabelle/sync-mod/Views.thy
@@ -1,10 +1,49 @@
 theory Views
-imports "../HOModel" 
+imports "../HOModel"
 begin
 
 typedecl Proc
 axiomatization where Proc_finite: "OFCLASS(Proc, finite_class)"
 instance Proc :: finite by (rule Proc_finite)
+
+datatype tree_2col =
+    Initial |
+    Node bool "tree_2col list"
+
+fun list_in where
+"list_in v [] = False" |
+"list_in v (a#l) = ((v = a) | list_in v l)"
+
+definition view_compute where
+"view_compute = (| 
+    CinitState = (%p st _. st = Initial),
+    sendMsg = (%_ _ st. st),
+    CnextState = (%_ s msgs _ s'. s' = Node
+        (EX p. msgs p = Bot)
+        (SOME l :: tree_2col list. ALL child :: tree_2col. ((list_in child l) = (EX p. msgs p = Content child))))
+|)"
+
+record locState = 
+     x :: nat
+     conc :: bool
+     ready :: bool
+     forc :: nat
+     level :: nat
+
+fun tree_to_state where
+"tree_to_state k Initial = (| x = 0, conc = False, ready = False, forc = 0, level = 0 |)" |
+"tree_to_state k (Node passiv_rcvd siblings) = (
+    let msgs = List.map (tree_to_state k) siblings in
+    let max_f = Max (forc ` (List.set msgs)) in
+    let v = Suc (LEAST v. EX s. list_in s msgs & x s = v) in 
+    let concr = ~ passiv_rcvd & (ALL s. list_in s msgs --> conc s) in
+    let readyr = ALL s. list_in s msgs --> ready s in
+    (| x = v, conc = concr | v mod k = 0, ready = readyr | v mod k = 0, forc = 0, level = 0 |))"
+
+definition view where
+"view rho HO p t 0 = Initial" |
+"view rho HO p t (Suc r) = (UN q : incoming rho HO p t r. if rho (t-r) q = Asleep then {q} else HO (t-r) q)"
+
 
 definition set_computable  where
 "set_computable f == EX algo comput. ALL rho HO r p s. HORun algo rho HO --> rho r p = Active s --> f rho HO p r = comput s"
